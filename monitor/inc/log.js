7bcc84fa96fc9e56c37cf126a5037d7579a538d0
Tony Gustafsson <mail@tonyg.se>
2018-10-26 19:54:50 +0200

    Use LokiJS for storing logs instead

--- monitor/inc/log.js
+++ monitor/inc/log.js
@@ -1,28 +1,39 @@
 'use strict';
 
 const moment = require('moment');
-const fs = require('fs');
+const loki = require('lokijs');
+
+var db = new loki('./log/log.json');
+var logTable = null;
+
+db.loadDatabase({}, () => {
+    logTable = db.getCollection('log');
+
+    if (logTable === null) {
+        logTable = db.addCollection('log', { indices: ['level', 'time'] });
+    }
+});
+
+let logToDb = (level = 'info', name, url, message, requestTime = 0) => {
+    let logObj = {
+        level: level,
+        time: moment.now(),
+        name: name,
+        url: url,
+        message: message,
+        requestTime: requestTime
+    };
+
+    logTable.insert(logObj);
+
+    db.saveDatabase(function(err) {
+        if (err) console.error('error saving');
+    });
+};
 
 const log = {
-    _log: (level = 'info', name, url, message, requestTime = 0) => {
-        let logObj = {
-            level: level,
-            time: moment.now(),
-            name: name,
-            url: url,
-            message: message,
-            requestTime: requestTime
-        };
-
-        let fileName = `log/${level}/${moment().format('YYYY-WW')}.json`;
-
-        fs.appendFile(fileName, '\n' + JSON.stringify(logObj), error => {
-            if (error) console.error(error);
-        });
-    },
-
     info: (name, url, message, requestTime) => {
-        log._log('info', name, url, message, requestTime);
+        logToDb('info', name, url, message, requestTime);
         console.log(`${moment().format('HH:mm:s')}: <${name}> ${message}`);
     },
 
@@ -30,7 +41,7 @@ const log = {
         let red = '\x1b[31m',
             reset = '\x1b[0m';
 
-        log._log('error', name, url, message, requestTime);
+        logToDb('error', name, url, message, requestTime);
         console.error(`${red}${moment().format('HH:mm:s')}: <${name}> ${message}${reset}`);
     }
 };
