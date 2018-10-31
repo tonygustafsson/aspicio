bbb55e4fb10fa46fbc7028c40f8be3477a13d07f
Tony Gustafsson <mail@tonyg.se>
2018-10-31 18:24:18 +0100

    Add moment js, use material cards for temp design

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import moment from 'moment';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
-const Status = ({ items, hasError, isLoading }) => {
+let locale = require('moment/locale/sv');
+moment.locale('sv', locale);
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper
+    },
+    gridList: {
+        width: '90%',
+        marginLeft: '5%',
+        height: 450
+    },
+    icon: {
+        color: 'rgba(255, 255, 255, 0.54)'
+    }
+});
+
+const Status = ({ items, hasError, isLoading, classes }) => {
     if (hasError) {
         return <p>Sorry! There was an error loading the items.</p>;
     }
@@ -14,18 +43,28 @@ const Status = ({ items, hasError, isLoading }) => {
     }
 
     return (
-        <div>
-            <h1>My statuses</h1>
-
-            {items.onlineServices.map(item => (
-                <div key={item.id}>
-                    <p>
-                        {item.name} Up: {item.time}
-                    </p>
-                </div>
-            ))}
+        <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+                <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div">Services</ListSubheader>
+                </GridListTile>
+                {items.onlineServices.map(service => (
+                    <GridListTile key={service.id}>
+                        <img src="img/server.png" alt={service.name} />
+                        <GridListTileBar
+                            title={service.name}
+                            subtitle={<span>{moment(service.time).format('LLLL')}</span>}
+                            actionIcon={
+                                <IconButton className={classes.icon}>
+                                    <InfoIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
         </div>
     );
 };
 
-export default Status;
+export default withStyles(styles)(Status);
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-
-import MyComponent from './components/MyComponent';
 import StatusContainer from './containers/StatusContainer';
 
 const initialState = {
@@ -18,7 +16,6 @@ render(
     <Provider store={store}>
         <div>
             <StatusContainer />
-            <MyComponent />
         </div>
     </Provider>,
     document.getElementById('app')
