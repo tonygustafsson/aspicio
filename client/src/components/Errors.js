36a3dc38a8e6974e5ce28dc533c6a0bff0d0e8c8
Tony Gustafsson <mail@tonyg.se>
2018-10-31 22:36:33 +0100

    Use nice table for errors

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import moment from 'moment';
 import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 let locale = require('moment/locale/sv');
 moment.locale('sv', locale);
 
-const styles = theme => ({});
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto'
+    },
+    table: {
+        minWidth: 700
+    },
+    td: {
+        textAlign: 'left'
+    }
+});
 
 const Errors = ({ errors, isLoading, classes }) => {
     if (!errors) {
@@ -17,15 +35,32 @@ const Errors = ({ errors, isLoading, classes }) => {
     }
 
     return (
-        <table>
-            {errors.map(error => (
-                <tr>
-                    <td>
-                        {moment(error.time).format('LLLL')}: {error.message}
-                    </td>
-                </tr>
-            ))}
-        </table>
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Time</TableCell>
+                        <TableCell>Service</TableCell>
+                        <TableCell>URL</TableCell>
+                        <TableCell>Message</TableCell>
+                        <TableCell>RequestTime (ms)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {errors.map(error => {
+                        return (
+                            <TableRow key={error.id}>
+                                <TableCell className={classes.td}>{moment(error.time).format('LLLL')}</TableCell>
+                                <TableCell className={classes.td}>{error.name}</TableCell>
+                                <TableCell className={classes.td}>{error.url}</TableCell>
+                                <TableCell className={classes.td}>{error.message}</TableCell>
+                                <TableCell className={classes.td}>{error.requestTime}</TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </Paper>
     );
 };
 
