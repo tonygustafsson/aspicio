2b464ac26383fbf0da8f9d4aca35cac23bd6be56
Tony Gustafsson <mail@tonyg.se>
2018-12-11 22:16:45 +0100

    Add fullscreen option in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import FullSceenIcon from '@material-ui/icons/Fullscreen';
 import green from '@material-ui/core/colors/green';
 import red from '@material-ui/core/colors/red';
 
@@ -12,6 +13,25 @@ const appBarStyle = hasErrors => {
     return {
         backgroundColor: hasErrors ? red[700] : green[700]
     };
+};
+
+const fullSceenIconStyle = {
+    position: 'absolute',
+    right: '20px'
+};
+
+const goFullScreen = () => {
+    var elem = document.documentElement;
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
 };
 
 const Header = ({ isAuthenticated, hasErrors }) => {
@@ -26,9 +46,14 @@ const Header = ({ isAuthenticated, hasErrors }) => {
                     <IconButton color="inherit" aria-label="Menu">
                         <MenuIcon />
                     </IconButton>
+
                     <Typography variant="h6" color="inherit">
                         Aspicio
                     </Typography>
+
+                    <IconButton style={fullSceenIconStyle} color="inherit" aria-label="Menu">
+                        <FullSceenIcon onClick={e => goFullScreen()} />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
         </Grid>
