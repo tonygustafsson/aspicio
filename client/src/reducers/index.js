5353563db60b994d699647ac4bc925275e3e4ca0
Tony Gustafsson <mail@tonyg.se>
2018-12-11 22:37:33 +0100

    What? Why?

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { data } from './status';
-import { toggleServiceState } from './service';
 import { isOnline } from './connectivity';
 import { isAuthenticated } from './auth';
 
 export default combineReducers({
     data,
-    toggleServiceState,
     isOnline,
     isAuthenticated
 });
