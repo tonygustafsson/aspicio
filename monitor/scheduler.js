73e113ee748f124b3fa3d41c8ddd6a737a1d3d84
Tony Gustafsson <mail@tonyg.se>
2018-12-12 19:31:36 +0100

    Change monitor scheduler schedule in config.json

--- monitor/scheduler.js
+++ monitor/scheduler.js
@@ -1,9 +1,8 @@
 const schedule = require('node-schedule');
 const { exec } = require('child_process');
+const config = require('../config.json');
 
-const cronSchedule = '*/10 * * * * *';
-
-schedule.scheduleJob(cronSchedule, function() {
+schedule.scheduleJob(config.monitorCheckServices, function() {
     exec('node monitor.js', (err, stdout) => {
         if (err) {
             console.error('Could not run monitor!');
