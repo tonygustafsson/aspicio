d34a7500e2a0415c3e3e651dc99d207bc92b63e2
Tony Gustafsson <mail@tonyg.se>
2018-12-12 19:25:36 +0100

    Allow everyone if token is empty

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -12,8 +12,13 @@ export function isAuthenticated() {
         var authToken = window.localStorage.getItem(constants.TOKEN.authTokenName);
 
         if (authToken && authToken === constants.TOKEN.authToken) {
+            // Auth if token matches constant
+            dispatch(authenticatedUser(true));
+        } else if (!authToken && constants.TOKEN.authToken === '') {
+            // Auth everyone if authToken is empty
             dispatch(authenticatedUser(true));
         } else {
+            // Do not auth, prevent from viewing client
             dispatch(authenticatedUser(false));
         }
     };
